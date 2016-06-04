52dd635aa64baba6ab4fd141d1f216402c32c160
barreta <sbarragan43@gmail.com>
2016-06-04 16:20:07 -0300

    Archivo inicial queries.java

--- src/main/java/bd2/util/Queries.java
+++ src/main/java/bd2/util/Queries.java
@@ -1,5 +1,33 @@
 package bd2.util;
 
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.cfg.Configuration;
+
+
 public class Queries {
 
+	private static SessionFactory sessions;
+	private static Transaction tx = null;
+
+	public Queries() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+
+		Configuration cfg = new Configuration();
+		cfg.configure("hibernate/hibernate.cfg.xml");
+		sessions = cfg.buildSessionFactory();
+		Session session = sessions.openSession();
+
+		/*
+		 * 
+		 * 
+		 */
+		
+		session.close();
+
+	}
 }
